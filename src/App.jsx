07692d2da75ac330c4ba67a31c9208291e812e30
Sovna Nayak <sovnanayak9@gmail.com>
2026-04-03 16:06:52 +0530

    page reloded and passanger completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 // import React from 'react';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 // import { Toaster } from "react-hot-toast";
+
 // import Navbar from './components/Navbar';
 // import Footer from './components/Footer';
 // import WhatsAppButton from "./components/WhatsAppButton";
 // import ChatWidget from "./components/ChatWidget";
+// import ScrollToTop  from './components/ScrollToTop';
 
 // import Home from './pages/HeroSection';
 // import Destinations from './pages/Destinations';
 // import Packages from './pages/Packages';
 // import Hotels from './pages/Hotel';
+// import HotelDetails from './pages/HotelDetails';
+// import BookingPage from './pages/BookingPage';
 // import Transport from './pages/Transport';
 // import FAQ from "./pages/FAQ";
 // import PrivacyPolicy from "./pages/PrivacyPolicy";
@@ -20,46 +24,68 @@
 // import Support from "./pages/Support";
 // import Contact from "./pages/Contact";
 // import BookingCalendar from './components/BookingCalendar';
-// import Rent from "./pages/Rent";
+// import Partner from './pages/Partner';
 // import Login from './pages/Login';
 // import Signup from './pages/Signup';
 
-// import './index.css';
-// // import { Contact } from 'lucide-react';
+// import hotels from "./data/hotelsData";
 
+// import './index.css';
+// import Partner from './pages/Partner';
+
+
+// /* 🔥 Separate component to use useLocation */
+// function AppContent() {
+//   const location = useLocation();
+
+//   return (
+   
+//     <div className="min-h-screen bg-gray-50 flex flex-col">
+//       <ScrollToTop />
+//       <Navbar />
+
+//       {/* ✅ Show only on Contact page */}
+//       {location.pathname === "/contact" && <WhatsAppButton />}
+//       {location.pathname === "/contact" && <ChatWidget />}
+
+//       <div className="flex-grow">
+//         <Routes>
+//           <Route path="/" element={<Home />} />
+//           <Route path="/destinations" element={<Destinations />} />
+//           <Route path="/packages" element={<Packages />} />
+//           <Route path="/hotels" element={<Hotels />} />
+//           <Route path="/hotel/:id" element={<HotelDetails />} />
+//           <Route path="/booking/:id" element={<BookingPage />} />
+//           <Route path="/transport" element={<Transport />} />
+//           <Route path="/partner" element={<Partner />} />
+//           <Route path="/faq" element={<FAQ />} />
+//           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+//           <Route path="/terms-of-use" element={<TermsOfUse />} />
+//           <Route path="/refund-cancellation" element={<RefundCancellation />} />
+//           <Route path="/about" element={<About />} />
+//           <Route path="/blog" element={<Blog />} />
+//           <Route path="/support" element={<Support />} />
+//           <Route path="/contact" element={<Contact />} />
+//           <Route path="/login" element={<Login />} />
+//           <Route path="/signup" element={<Signup />} />
+          
+//         </Routes>
+//       </div>
+
+//       <Footer />
+
+//       {/* Toast */}
+//       <Toaster position="top-right" />
+//     </div>
+//   );
+// }
+
+
+// /* 🔥 Main App */
 // function App() {
 //   return (
 //     <Router>
-//       <div className="min-h-screen bg-gray-50 flex flex-col">
-
-//         <Navbar />
-//         <WhatsAppButton />   {/* 👈 yaha add karo */}
-//         <ChatWidget />
-//         <div className="flex-grow">
-//           <Routes>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/destinations" element={<Destinations />} />
-//             <Route path="/packages" element={<Packages />} />
-//             <Route path="/hotels" element={<Hotels />} />
-//             <Route path="/transport" element={<Transport />} />
-//             <Route path="/rent" element={<Rent />} />
-//             <Route path="/faq" element={<FAQ />} />
-//             <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-//             <Route path="/terms-of-use" element={<TermsOfUse />} />
-//             <Route path="/refund-cancellation" element={<RefundCancellation />} />
-//             <Route path="/about" element={<About />} />
-//             <Route path="/blog" element={<Blog />} />
-//             <Route path="/support" element={<Support />} />
-//             <Route path="/contact" element={<Contact />} />
-//             <Route path="/login" element={<Login />} />
-//             <Route path="/signup" element={<Signup />} />
-            
-//           </Routes>
-//         </div>
-
-//         <Footer />
-
-//       </div>
+//       <AppContent />
 //     </Router>
 //   );
 // }
@@ -75,13 +101,12 @@
 
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { Toaster } from "react-hot-toast";
 
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import WhatsAppButton from "./components/WhatsAppButton";
 import ChatWidget from "./components/ChatWidget";
-import ScrollToTop  from './components/ScrollToTop';
+import ScrollToTop from './components/ScrollToTop';
 
 import Home from './pages/HeroSection';
 import Destinations from './pages/Destinations';
@@ -98,24 +123,19 @@ import About from "./pages/About";
 import Blog from "./pages/Blog";
 import Support from "./pages/Support";
 import Contact from "./pages/Contact";
-import BookingCalendar from './components/BookingCalendar';
 import Partner from './pages/Partner';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 
-import hotels from "./data/hotelsData";
-
 import './index.css';
-import Partner from './pages/Partner';
-
 
 /* 🔥 Separate component to use useLocation */
 function AppContent() {
   const location = useLocation();
 
   return (
-   
     <div className="min-h-screen bg-gray-50 flex flex-col">
+      
       <ScrollToTop />
       <Navbar />
 
@@ -143,18 +163,13 @@ function AppContent() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          
         </Routes>
       </div>
 
       <Footer />
-
-      {/* Toast */}
-      <Toaster position="top-right" />
     </div>
   );
 }
-
 
 /* 🔥 Main App */
 function App() {
